2e73ab933d46d14174514868674f9e496ffb32c1
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-03-16 10:10:06 +0100

    Añadida Excepción para control de 404

---
+++ src/main/java/com/proyecto/app/entity/ExcepcionNoEncontrado.java
@@ -0,0 +1,19 @@
+package com.proyecto.app.entity;
+
+import org.springframework.http.ResponseEntity;
+
+import javax.swing.*;
+
+/**
+ * Creado por @autor: angel
+ * El  16 de mar. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class ExcepcionNoEncontrado extends Exception{
+    public ExcepcionNoEncontrado() {
+        super();
+    }
+    public ExcepcionNoEncontrado(ResponseEntity mensaje){
+        super("Error"+ mensaje);
+    }
+}
