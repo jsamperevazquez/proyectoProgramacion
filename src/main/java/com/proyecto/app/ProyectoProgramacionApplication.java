2f49bc17f24086d579b31ca9268f7fbb2f5ae8a9
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-03-17 21:52:12 +0100

    Arreglado error

--- src/main/java/com/proyecto/app/ProyectoProgramacionApplication.java
+++ src/main/java/com/proyecto/app/ProyectoProgramacionApplication.java
@@ -8,6 +8,7 @@ public class ProyectoProgramacionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoProgramacionApplication.class, args);
+
     }
 
 }
