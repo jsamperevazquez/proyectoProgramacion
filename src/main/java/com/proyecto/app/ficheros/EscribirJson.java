be9da22a72b152b0e2918f7daaa2c3dd252b2ab7
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-03-17 11:37:43 +0100

    Creo clases para lectura y escritura de JSON en ficheros

---
+++ src/main/java/com/proyecto/app/ficheros/EscribirJson.java
@@ -0,0 +1,9 @@
+package com.proyecto.app.ficheros;
+
+/**
+ * Creado por @autor: angel
+ * El  17 de mar. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class EscribirJson {
+}
